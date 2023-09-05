33cabd5e7abea7e5ebc70e78dbdba6ae8353872b
victorvlte <142946764+victorvlte@users.noreply.github.com>
2023-09-04 21:15:39 -0300

    Add files via upload

--- Ex01.java
+++ Ex01.java
@@ -8,14 +8,21 @@ import java.util.Scanner;
 public class Ex01 {
 public static void main(String[] args) {
     Scanner entrada = new Scanner(System.in);
-    float numero1, numero2;
+    float numero1, numero2, media;
 
     System.out.println("Digite um número:");
     numero1 = entrada.nextFloat();
     System.out.println("Digite o segundo número");
     numero2 = entrada.nextFloat();
+    media= (numero1 + numero2)/2;
+    System.out.println("A média aritmética é:"+(media));
 
-    System.out.println("A média aritmética é:"+((numero1 + numero2)/2));
-    
+
+    if (media >= 7)
+        System.out.println("Parabéns você foi aprovado!");  
+    else
+        System.out.println("Você não atingiu a pontuação necessária, tente novamente!");  
+
+        System.out.println("Final do Programa");
 }
 }
