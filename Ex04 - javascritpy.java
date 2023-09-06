aac0b0e2abcdc1fe8ab0032d62a61080bcf05915
victorvlte <142946764+victorvlte@users.noreply.github.com>
2023-09-05 21:50:06 -0300

    Add files via upload

---
+++ Ex04 - javascritpy.java
@@ -0,0 +1,26 @@
+package ex01;
+
+import java.util.Scanner;
+
+public class Ex04 { 
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int pedido, p1, p2, p3, p4;
+        System.out.println("Lanchonete Burgão");
+        System.out.println("1 - Suco R$ 2,50");
+        System.out.println("2 - Refri R$ 7,00");        
+        System.out.println("3 - HOT DOG R$ 12,00");
+        System.out.println("4 - HAMBURGUER R$ 18,00");
+        
+        System.out.println("Faça o seu pedido, digite o numero:");
+
+        pedido= entrada.nextInt();
+
+        switch;pedido{}
+
+        
+
+
+
+    
+}
