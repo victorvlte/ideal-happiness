64b72a642393fe7e09e1b51c766d5d00676d77f8
victorvlte <142946764+victorvlte@users.noreply.github.com>
2023-09-17 19:10:51 -0300

    Add files via upload

--- Projetohamburgueria.java
+++ Projetohamburgueria.java
@@ -1,28 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package projetohamburgueria;
-import java.util.Scanner;
-public class Projetohamburgueria {
+   package projetohamburgueria;
+    import java.util.Scanner;
+    public class Projetohamburgueria {
 
        public static void main(String[] args) {
         
             Scanner entrada = new Scanner(System.in);
-            int opcao;
+            int opcao, i=0;
             char valor;
+            int[] carrinho = new int[10];
+            
             System.out.println("----Anhembi Lanches ----");
             System.out.println("Cardapio");
             System.out.println("1 Suco        R$ 2,00 ");
             System.out.println("2 - Refri     R$ 7,00");
-            System.out.println("3 - Hotdoh    R$ 12,50");
+            System.out.println("3 - HotdoG    R$ 12,50");
             System.out.println("4 - Hamburguer R$ 19,20");
             
             do {
                 System.out.println("Digite seu pedido");
                 
                 opcao = entrada.nextInt();
+                
                 switch (opcao) { 
+                    
                     case 1:
                         System.out.println("Voce escolheu suco");
                         break;
@@ -40,11 +40,46 @@ public class Projetohamburgueria {
                         
                            
                 }
-                
+                    carrinho[i] = opcao;
                     System.out.println("Deseja incluir outro item? (s ou n) ");
-                    opcao = entrada.next().charAt (0);
+                    
                     valor = entrada.next().charAt (0);
+                    i++;
             }       while (valor != 'n');
+                    
+                    
+                    //for(int j=0; j<10;j++) {
+                    for( i=0; i<10;i++) {
+                        
+                    switch (carrinho [i]){
+                        
+                        case 1:
+                        System.out.println("Você escolheu suco");
+                        break;
+                        
+                        case 2:
+                        System.out.println("Você escolheu Refri");
+                        break;
+                        
+                        case 3:
+                        System.out.println("Voce escolher Hot Dog");
+                        break;
+                        
+                        case 4:
+                        System.out.println("Você escolheu hamburguer");
+                        break;
+                        
+                        //default:
+                           
+                    }
+                          
+                        
+                    
+                        
+                        
+                        
+                    }
+                           
     }
        
 }
