78fa1ef822a24d74eb901651114ddc84b36f0cbb
victorvlte <142946764+victorvlte@users.noreply.github.com>
2023-09-24 11:18:09 -0300

    Add files via upload

---
+++ EX.  WHILE  - Contador01.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package contador01;
+
+/**
+ *
+ * @author Victor
+ */
+public class Contador01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int cc=0;
+        
+        while (cc<10) {
+            cc++;
+            if (cc == 2 || cc == 3 ||cc== 4) {
+                continue;
+            }
+            if (cc == 7) {
+                break;
+            }
+            System.out.println("Cambalhota " + (cc));
+            
+            
+        }
+        
+    }
+    
+}
