eb6cb3387b66b94a8b0abdec6ae9f1b8571d36f8
victorvlte <142946764+victorvlte@users.noreply.github.com>
2023-10-17 21:19:04 -0300

    Add files via upload

--- ex01.java
+++ ex01.java
@@ -1,24 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package aulaterca;
+package treinoa1;
+import java.util.Random;
 
-/**
- *
- * @author profslvo
- */
 public class ex01 {
-    public static void main(String[] args) {
-        
-        for (int numero = 0; numero <= 50; numero++){
+
+        /**
+         * @param args
+         */
+        public static void main(String[] args) {
+            Random aleatorios = new Random();
             
-            System.out.println(numero);
+            int vetInt [] = new int [15];
+            
+            int maior=0,menor=0;
+            
+            for (int i = 0; i < 15; i++) {
+                vetInt[i] = aleatorios.nextInt(10);
+                if(i==0){
+                    maior= vetInt[i];
+                    menor= vetInt[i];
+                }
+
+                
+
+            }   
+
+            for (int i = 0; i < 15; i++) {
+                if(vetInt[i]> maior){
+                    maior= vetInt[i];
+
+                }
+                if(vetInt[i]<menor){
+                    menor = vetInt[i];
+                }
+            }
+                for (int i = 0; i < 15; i++) {
+                    System.out.printf("%d \t",vetInt[i]);
+                    
+                }
+
+            
+                System.out.printf("\n");
+            System.out.println("O maior valor e:  "+maior);
+            System.out.println("O menor valor e:  " +menor);
+
+        
         }
-            
-            
-            
-            
-        
-    }
+    
 }
