5c0dd6d14df838784d4632713abf3c909274d9a7
victorvlte <142946764+victorvlte@users.noreply.github.com>
2023-10-23 21:26:41 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,27 @@
+package exercicios;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        
+        Estudante e1 = new Estudante(111, "Carlos","ADS");
+        Pessoa p = new Pessoa(123, "Kleber");    
+        
+        //System.out.println("Registro "+ p.getRegistro());
+        //System.out.println("Pessoa "+ p.getNome());
+        //e1.setNomeCurso("ADS");
+        //e1.setNome("Carlos");
+        System.out.println("Registro: " + e1.getRegistro());
+        System.out.println("Estudante: " + e1.getNome());
+        System.out.println("Curso: " + e1.getNomeCurso());
+
+           System.out.println("....");
+
+           System.out.println(p);
+           
+           System.out.println(e1); 
+    }
+
+    
+}
---
+++ Estudante.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+public class Estudante extends Pessoa {
+   
+    private String nomeCurso;
+
+    
+    
+    public Estudante(int registro, String nome, String nomeCurso){
+        super(registro,nome);
+        this.nomeCurso=nomeCurso;
+    }
+    
+    public String getNomeCurso() {
+        return nomeCurso;
+    }
+
+    public void setNomeCurso(String nomeCurso) {
+        this.nomeCurso = nomeCurso;
+    }
+     
+    @Override
+    public String toString(){
+    return "Estudante " + getRegistro()+ " : " + getNome() + " : " +nomeCurso; 
+
+    }
+   
+}
