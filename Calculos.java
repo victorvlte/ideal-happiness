b867e19d24a52e058ea690eac7a97a809201baee
victorvlte <142946764+victorvlte@users.noreply.github.com>
2023-11-07 21:37:05 -0300

    Add files via upload

---
+++ Calculos.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculossoma;
+
+/**
+ *
+ * @author profslvo
+ */
+public class Calculos {
+    
+    public int somar(int soma1, int soma2){
+       return   soma1+soma2;
+    }
+    
+    public double somarD(double somad1, double somad2){
+       return  somad1+somad2;
+    } 
+    
+    public float somarF(float somaf1,float somaf2){
+       return  somaf1+somaf2;
+    }
+}
+    
---
+++ CalculosSoma.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculossoma;
+
+/**
+ *
+ * @author profslvo
+ */
+public class CalculosSoma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+      Calculos somar = new Calculos();
+      
+       
+        
+    }
+    
+}
